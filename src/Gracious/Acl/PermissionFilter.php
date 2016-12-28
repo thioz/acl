5e6b053c8814a34a0a6c798294279c8180e265ee
Rob Theeuwes <rob@graciousstudios.nl>
2016-12-28 13:42:15 +0100

    renamed Resource to AclResource for PHP7 compat

--- src/Gracious/Acl/PermissionFilter.php
+++ src/Gracious/Acl/PermissionFilter.php
@@ -2,6 +2,11 @@
 namespace Gracious\Acl;
 
 abstract  class PermissionFilter {
-	
+
+	/**
+	 * @param AclIdentityInterface $identity
+	 * @param null $context
+	 * @return boolean
+	 */
 	abstract function check(AclIdentityInterface $identity, $context = null);
 }
--- src/Gracious/Acl/PermissionRequest.php
+++ src/Gracious/Acl/PermissionRequest.php
@@ -2,11 +2,24 @@
 namespace Gracious\Acl;
 
 class PermissionRequest {
+
+	/**
+	 * @var string
+	 */
 	public $resource;
+
+	/**
+	 * @var string
+	 */
 	public $perm;
+
+	/**
+	 * @var int|string
+	 */
 	public $contextId;
 	
 	public function __construct($resource,$perm,$contextId) {
+
 		$this->resource=$resource;
 		$this->perm=$perm;
 		$this->contextId=$contextId;
